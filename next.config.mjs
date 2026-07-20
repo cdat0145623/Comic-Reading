7f149c7ea4913f8006ea39da29f5805c11832c07
NguyenChauQuocDat <cdat789@gmail.com>
2026-07-20 11:17:55 +0700

    chore(project): add tooling and environment templates

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["static.cdnno.com", "lh3.googleusercontent.com"],
+        domains: [
+            "static.cdnno.com",
+            "lh3.googleusercontent.com",
+            "res.cloudinary.com",
+            "authjs.dev",
+        ],
     },
 };
 
